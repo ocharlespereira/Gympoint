80024e992cd9b1b298ebd3b890c7dfecd9c54292
Charles Pereira <charlesti.ptu@gmail.com>
2019-10-23 09:50:04 -0300

    Correção de condição checkins intervalo de datas e rotas

--- backend/src/app/controllers/CheckinController.js
+++ backend/src/app/controllers/CheckinController.js
@@ -1,68 +1,70 @@
 import { Op } from 'sequelize';
-import { subDays, endOfDay } from 'date-fns';
+import { subDays, endOfday } from 'date-fns';
+
 import Student from '../models/Student';
 import Checkin from '../models/Checkin';
 
 class CheckinController {
-  async index(req, res) {
-    const { studentId } = req.params;
-
-    if (!studentId) {
-      return res.status(400).json({ error: 'Student not provided' });
-    }
-
-    const student = await Student.findByPk(studentId);
-
-    if (!student) {
-      return res.status(400).json({ error: 'Student does not exist' });
-    }
-
-    const checkins = await Checkin.findAll({
-      where: { student_id: student.id },
-      attributes: ['id', 'student_id', 'created_at'],
-    });
-
-    return res.json(checkins);
-  }
-
-  async store(req, res) {
-    const { studentId } = req.params;
-
-    if (!studentId) {
-      return res.status(400).json({ error: 'Student not provided' });
-    }
-
-    const student = await Student.findByPk(studentId);
-
-    if (!student) {
-      return res.status(400).json({ error: 'Student does not exist' });
-    }
-
-    // 5 checkins within last 7 days
-    const seventhDayAgo = subDays(new Date(), 7);
-    const today = endOfDay(new Date());
-
-    const checkins = await Checkin.count({
-      where: {
-        student_id: student.id,
-        created_at: {
-          [Op.between]: [seventhDayAgo, today],
-        },
-      },
-    });
-
-    if (checkins >= 5) {
-      return res
-        .status(400)
-        .json({ error: 'Student already has 5 checkins in the last 7 days' });
-    }
-
-    const { id, student_id } = await Checkin.create({
-      student_id: student.id,
-    });
-
-    return res.json({ id, student_id });
-  }
+	async index(req, res) {
+		async { studentId } = req.params;
+		
+		if (!studentId) {
+			return res.status(400).json({ error: 'Student not provided.'})
+		}
+		
+		const student = await Student.findByPk(studentId);
+		
+		if (!student) {
+			return status(400).json({ error: 'Student does not exist.' })
+		}
+		
+		const checkins = await Checkin.findAll({
+			where: { student_id: student.id },
+			attributes: ['id', 'student_id', 'created_at'],
+		});
+		
+		return res.json(checkins);
+	}
+	
+	async store(req, res) {
+		const { studentId } = req.paramns;
+		
+		if (!studentId) {
+			return res.status(400).json({ error: 'Student not provided.'})
+		}
+		
+		const student = await Student.findByPk(studentId);
+		
+		if (!student) {
+			return status(400).json({ error: 'Student does not exist.' })
+		}
+		
+		// 5 checkins whith last 7 days
+		const seventhDayAgo = subDays(new Date(), 7);
+		const today = endOfDay(new Date());
+		
+		// Query of dates between seventhDayAgo(actual date plus 7 days) and today(actual date)
+		const checkins = await Checkin.count({
+			where: 
+				{ student_id: student.id,
+				created_at: {
+					[Op.between]: [seventhDayAgo, today],
+				}
+			}
+		});
+		
+		// Verify if string checkins if student entry is greater than or equal to 5
+		if (checkins >= 5) {
+			return res.status(400).json({error: 'Student already has 5 checkins in the last 7 days. . Access denied!'})
+		}
+		
+		const { id, student_id } = await Checkin.create({
+			student_id: student_id
+		});
+		
+		return res.json({ id, student_id })
+		
+	}
 }
 
 export default new CheckinController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -18,32 +18,37 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+// Account
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+//User
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
 
+// Student
 routes.get('/students', StudentController.index);
 routes.post('/create', StudentController.store);
 routes.put('/editStudents/:id', StudentController.update);
 
+// File Image
 routes.post('/files', upload.single('file'), FileController.store);
 
+// Plans
 routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
 routes.put('/plans/:id', PlanController.update);
 routes.delete('/plans/:id', PlanController.delete);
 
+// Registration
 routes.get('/registrations', RegistrationController.index);
 routes.post('/registrations', RegistrationController.store);
 routes.delete('/registrations/:id', RegistrationController.delete);
 
-routes.get('/checkins', CheckinController.index);
-routes.post('/checkins', CheckinController.store);
-
-routes.get('/notifications', NotificationController.index);
+// Checkins
+routes.get('/students/:studentId/checkins', CheckinController.index);
+routes.post('/students/:studentId/checkins', CheckinController.store);
 
 export default routes;
