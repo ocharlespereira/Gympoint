0f0098203309fed33d8e026195ec02bb8aa81df3
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-05 22:41:26 -0300

    troca dod eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,25 +4,40 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb',
     'prettier',
+    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: [
+    'react',
     'prettier',
+    'react-hooks',
   ],
   rules: {
-    "prettier/prettier": 'error',
-    "class-methods-use-this": 'off',
-    "no-param-reassign": 'off',
-    "camelcase": 'off',
-    "no-unused-vars": ['error', { 'argsIgnorePattern': 'next' }],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off',
+    "react/state-in-constructor": 'off',
+    "react/static-property-placement": 'off',
+    'no-console': ["error", { allow: ["tron"] }],
+    'no-param-reassign': 'off',
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
