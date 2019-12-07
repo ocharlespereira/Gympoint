99df2e4dd78420b8b59a4bc58d264609d6ca5e6f
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-07 00:46:52 -0300

    Configurando Reactotron

--- frontend/src/pages/Plan/index.js
+++ frontend/src/pages/Plan/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Plan() {
-  return <div />;
+  return <h1>PLANOS</h1>;
 }
--- frontend/src/pages/Registration/index.js
+++ frontend/src/pages/Registration/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Registration() {
-  return <div />;
+  return <h1>MATRICULAS</h1>;
 }
