effd4e3397b570e7723f5dc4b46922d5febd72d9
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-07 01:06:11 -0300

    Rotas privadas

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 
@@ -14,11 +15,14 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
 
-      <Route path="dashboard" component={Dashboard} />
-      <Route path="/studendent" component={Student} />
-      <Route path="/plan" component={Plan} />
-      <Route path="/registration" component={Registration} />
-      <Route path="/request" component={Request} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/studendent" component={Student} isPrivate />
+      <Route path="/plan" component={Plan} isPrivate />
+      <Route path="/registration" component={Registration} isPrivate />
+      <Route path="/request" component={Request} isPrivate />
+
+
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
