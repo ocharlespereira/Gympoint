f8f3c7d57c8dc23d3a182908d9df3de73b436c05
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-07 03:15:16 -0300

    Estilizando tela de Login Gympoint

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import logo from '~/assets/logo.svg';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>LOGIN</h1>;
+  return (
+    <>
+      <img src={logo} alt="" />
+
+      <form>
+        <span>SEU E-MAIL</span>
+        <input type="email" placeholder="exemplo@email.com" />
+
+        <span>SUA SENHA</span>
+        <input type="password" placeholder="****************" />
+
+        <button type="submit">Entrar no Sistema</button>
+      </form>
+    </>
+  );
 }
--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -1,6 +1,63 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #ee4d64, #ff7a8d);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 360px;
+  height: 448px;
+  background: #fff;
+  border-radius: 4px;
+
+  img {
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
+    padding: 40px 15px;
+  }
+
+  form {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    margin: 0 30px;
+  }
+
+  span {
+    color: #444;
+    margin: 0 0 2px;
+    font-weight: bold;
+    padding: 0 1px;
+  }
+
+  input {
+    color: #999;
+    width: 300px;
+    height: 45px;
+    border-radius: 4px;
+    margin-bottom: 15px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+  }
+
+  button {
+    margin: 5px 0 0;
+    height: 44px;
+    background: #ee4d64;
+    font-weight: bold;
+    color: #fff;
+    border: 1px #ddd;
+    border-radius: 4px;
+
+    &:hover {
+      background: ${darken(0.03, '#ff7a8d')};
+    }
+  }
 `;
