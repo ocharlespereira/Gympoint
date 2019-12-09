76508d70b11ac44ce73e443f09eb50318e877eb1
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-08 22:55:42 -0300

    Utilizando Unform @RocketSeat Reactotron

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform'
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
-
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <span>SEU E-MAIL</span>
-        <input type="email" placeholder="exemplo@email.com" />
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
 
         <span>SUA SENHA</span>
-        <input type="password" placeholder="****************" />
+        <Input name="passwordd" type="password" placeholder="****************" />
 
         <button type="submit">Entrar no Sistema</button>
-      </form>
+      </Form>
     </>
   );
 }
