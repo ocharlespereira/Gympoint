379ec4cfa21e971d5aef61334d208c8ff63d7724
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-28 23:56:21 -0300

    Store user-sagas

--- frontend/src/store/modules/user/sagas.js
+++ frontend/src/store/modules/user/sagas.js
@@ -1,3 +1,30 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+function* updateProfile({ payload }) {
+    try {
+        const { name, email, avatar_id, ...rest } = payload.data;
+
+        const profile = {
+            name,
+            email,
+            avatar_id,
+            ...payload(rest.oldPassword ? rest : {}),
+        };
+
+        const response = yield call (api.put, 'users', profile);
+
+        toast.success('Perfil Atualizado com sucesso.');
+
+        yield put(updateProfileSuccess());
+    }catch (error) {
+        toast.error('Erro ao atualizar perfil, confira seus dados.');
+        yield put(updateProfileFailure());
+    }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
