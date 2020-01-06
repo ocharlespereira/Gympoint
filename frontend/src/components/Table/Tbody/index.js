a503fa397e6d9210bcb3828beaa6393163ca77bb
Charles Pereira <charlesti.ptu@gmail.com>
2020-01-05 23:19:04 -0300

    Component Table - Tbody index e style

--- frontend/src/components/Table/Tbody/index.js
+++ frontend/src/components/Table/Tbody/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
 
-// import { Container } from './styles';
+import { CustomTbody } from './styles';
 
-export default function Tbody() {
-  return <div />;
+export default function Tbody({ children, align, ...rest }) {
+  return (
+    <CustomTbody align={align} {...rest}>
+      {children}
+    </CustomTbody>
+  );
 }
+
+Tbody.defaultProps = {
+  align: 'left',
+}
+
+Tbody.PropsTypes = {
+  children: PropsTypes.oneOfType([
+    PropsTypes.arrayOf(PropsTypes.element),
+    PropsTypes.element,
+  ]).isRequired,
+  align: PropsTypes.string,
+};
---
+++ frontend/src/components/Table/Tbody/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const CustomTbody = styled.thead`
+  text-align: ${props => props.align};
+`;
