014a4d2228a8ac3b2b9f270a9c75ec9855ab9e11
Charles Pereira <charlesti.ptu@gmail.com>
2020-01-07 23:15:22 -0200

    components Input

---
+++ frontend/src/components/InputMask/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class InputMask extends Component {
+  render() {
+    return <div />;
+  }
+}
