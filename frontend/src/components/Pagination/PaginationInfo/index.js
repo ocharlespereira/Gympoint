93f2a126eae1246284a262298692777c547cbdf5
Charles Pereira <charlesti.ptu@gmail.com>
2020-01-12 17:31:35 -0300

    Component - Pagination index e styles

---
+++ frontend/src/components/Pagination/PaginationInfo/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/require-default-props */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Container } from './styles';
+
+export default function PaginationInfo({ page, perPage, totalPage, total }) {
+  return (
+    <Container>
+      <strong>Total: {total}</strong>
+      <span>
+        Página: {page}/{totalPage}itens por página: {perPage}
+      </span>
+    </Container>
+  );
+}
+
+PaginationInfo.defaultsProps = {
+  page: 0,
+  perPage: 0,
+  totalPage: 0,
+  total: 0,
+};
+
+PaginationInfo.propTypes = {
+  page: PropTypes.number,
+  perPage: PropTypes.number,
+  totalPage: PropTypes.number,
+  total: PropTypes.number,
+};
---
+++ frontend/src/components/Pagination/PaginationInfo/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  strong {
+    color: #333;
+  }
+
+  span {
+    color: #333;
+  }
+`;
