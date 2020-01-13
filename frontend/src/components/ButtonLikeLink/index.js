b754ad5f57de844a367c0f3f5ebcecef3742d932
Charles Pereira <charlesti.ptu@gmail.com>
2020-01-12 22:01:26 -0300

    Component - ButtonLikeLink index e styles

--- frontend/src/components/ButtonLikeLink/index.js
+++ frontend/src/components/ButtonLikeLink/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
 import colors from '~/styles/colors';
---
+++ frontend/src/components/ButtonLikeLink/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const CustomButtonLikeLink = styled.button`
+  background-color: transparent;
+  border: none;
+  display: inline;
+  color: ${props => props.color};
+`;
